045517b5a02d230c4b786f4ced5b314fa02adc09
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2023-11-01 20:22:40 -0400

    child theme

---
+++ themes/twentytwentythree-child/functions.php
@@ -0,0 +1,20 @@
+<!-- I used a wrong theme in the last assignment and it doen't contain
+a functions.php, I tested the child theme from the a -->
+
+<?php
+    // @link( 'https://developer.wordpress.org/themes/basics/theme-functions/' );
+
+    // @package ('WordPress');
+
+    // @subpackage ('Twenty_Twenty_Three');
+    // @since ('Twenty Twenty Three 1.0');
+
+
+    // add_action( 'wp_enqueue_scripts', 'twentytwentythreechild_wp_enqueue_scripts' );
+    // function twentytwentythreechild_wp_enqueue_scripts() {
+    //     wp_enqueue_style( 'twentytwentythreechild-style', 
+    //     get_stylesheet_uri(), array( 'twenty-twenty-three-style' ), 
+    //     wp_get_theme()->get('Version') 
+    //     );
+    // }
+?>
